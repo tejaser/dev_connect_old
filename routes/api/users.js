29b7a58890c4defc5b8cabb058b37ddaa2aab570
Tejas Pancholi <tejas.er@outlook.com>
2018-05-24 13:08:29 +0530

    added model and user registration route.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,53 @@
 const express = require("express");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
 const router = express.Router();
+
+// Load user model
+
+const User = require("../../models/User");
 
 // @route   GET api/users/test
 // @desc    Tests post route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "users works" }));
+
+// @route   GET api/users/register
+// @desc    Register a user
+// @access  Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: "Email already exists." });
+      } else {
+        const avatar1 = gravatar.url(req.body.email, {
+          s: "200", // Size of avatar
+          r: "pg", // Rating of the avatar
+          d: "mm" // Default image
+        });
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar: avatar1,
+          password: req.body.password
+        });
+        console.log(newUser);
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            console.log(err);
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    })
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 
 const app = express();
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // DB config
 const db = require("./config/keys").mongoURI;
 
